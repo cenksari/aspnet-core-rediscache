9e238487ad6e4537a55c2cc4c93db9e2879c78da
Cenk SARI <cenk@cenksari.com>
2022-06-01 13:08:39 +0300

    Net 6.0 update

--- aspnet-core-rediscache/CacheService.cs
+++ aspnet-core-rediscache/CacheService.cs
@@ -3,6 +3,7 @@
 	using Microsoft.Extensions.Caching.Distributed;
 	using System;
 	using System.Text.Json;
+	using System.Text.Json.Serialization;
 	using System.Threading.Tasks;
 
 	/// <summary>
@@ -13,6 +14,11 @@
 		public readonly IDistributedCache Cache;
 
 		public CacheService(IDistributedCache cache) => Cache = cache;
+
+		private readonly JsonSerializerOptions options = new()
+		{
+			DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+		};
 
 		/// <summary>
 		/// Get cache value.
@@ -33,7 +39,7 @@
 		/// <param name="minutes">Minutes to keep</param>
 		public async Task<bool> SetCacheAsync<T>(string key, T value, int minutes) where T : class
 		{
-			string response = JsonSerializer.Serialize(value);
+			string response = JsonSerializer.Serialize(value, options);
 
 			await Cache.SetStringAsync(key, response, new DistributedCacheEntryOptions { SlidingExpiration = TimeSpan.FromMinutes(minutes) });
 
